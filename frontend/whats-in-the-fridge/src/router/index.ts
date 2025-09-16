ed6c8874edbe8787308dee7f8d8820f854a7c8b1
Nicolas Benavides Linares <nbl0357@gmail.com>
2025-09-16 16:20:08 -0400

    Add routes

--- frontend/whats-in-the-fridge/src/router/index.ts
+++ frontend/whats-in-the-fridge/src/router/index.ts
@@ -1,8 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+// Views
+import App from '../App.vue'
+import AuthView from '../views/AuthView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: App,
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView,
+    },
+  ],
 })
 
 export default router
